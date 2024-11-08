3dc56042993dbac9ddfebf62814dc8b6fb114e71
1chary <abhilashchary04@gmail.com>
2024-11-08 12:38:20 +0530

    getting the user data if exists in the database and generating jwt token

--- app.js
+++ app.js
@@ -1,13 +1,38 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const http = require("http");
 const {open} = require("sqlite");
-const sqlite3 = require("sqlite3")
-const dbPath = path.join(__dirname,"userdata.db")
+const sqlite3 = require("sqlite3");
+const dbPath = path.join(__dirname,"userdata.db");
+const jwt = require("jsonwebtoken");
 
 let db = null;
 
+{/* Validating user if exists it will generate jwt token else throws an error */}
+app.get("/userdata/:username", async (request,response) => {
+    const {username} = request.params 
+    const getUserData = `
+    SELECT *
+    FROM user_details
+    where name = '${username}'
+    `;
+    const queryResult = await db.get(getUserData)
+    if (queryResult === undefined) {
+        response.send("user not available")
+    }
+    else {
+        const payLoad = {
+            username: username
+        }
+        const jwtToken = jwt.sign(payLoad,"my_token")
+        response.send(jwtToken)
+    }
+})
+
+{/* Retrieve loan details*/}
+app.get("/loanDetails", async(request,response) => {
+    
+})
 
 
 const initializeTheDBAndServer = async () => {
